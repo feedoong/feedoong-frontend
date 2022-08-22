fc98de743f9f32ee510d2e5e358448a80d3e92bc
Yeongjae-Shin <syj9484@gmail.com>
2022-08-22 22:34:49 +0900

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from "react-query";
+import { RecoilRoot } from "recoil";
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </QueryClientProvider>
+  )
 }
 
 export default MyApp
