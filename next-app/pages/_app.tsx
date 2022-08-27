eaec72d66e36f65f2fc262eb168d0dc7751e0a13
saengmotmi <ohjtack@gmail.com>
2022-08-27 16:21:03 +0900

    font 세팅 wip

--- next-app/pages/_app.tsx
+++ next-app/pages/_app.tsx
@@ -1,16 +1,30 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { RecoilRoot } from 'recoil'
+
+import '../styles/reset.css'
+import '../styles/font.css'
 
 const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <Component {...pageProps} />
-      </RecoilRoot>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          as="style"
+          crossOrigin="anonymous"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/variable/pretendardvariable.css"
+        />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </QueryClientProvider>
+    </>
   )
 }
 
