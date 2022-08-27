e8eaac51104ad343e9a35e42b5e6ee4498f2d550
saengmotmi <ohjtack@gmail.com>
2022-08-27 17:17:35 +0900

    폴더 구조 변경 / 절대 경로 적용

--- next-app/src/pages/_app.tsx
+++ next-app/src/pages/_app.tsx
@@ -3,8 +3,8 @@ import Head from 'next/head'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { RecoilRoot } from 'recoil'
 
-import '../styles/reset.css'
-import '../styles/font.css'
+import 'styles/reset.css'
+import 'styles/font.css'
 
 const queryClient = new QueryClient()
 
