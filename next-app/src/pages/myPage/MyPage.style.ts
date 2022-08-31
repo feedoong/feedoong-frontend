77f8b1aea515c653a439551aa3c62d2ba7a11ff2
Eunsun Shin <unusual0218@gmail.com>
2022-08-31 23:36:32 +0900

    Add: 마이페이지 생성 및 레이아웃 설정

---
+++ next-app/src/pages/myPage/MyPage.style.ts
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  /* display: flex;
+  justify-content: center; */
+  background-color: #212322;
+`;
+
+export const Contents = styled.div`
+  width: 626px;
+  margin: 0 auto;
+  padding-top: 120px;
+  /* border: 1px solid red; */
+`;
+
+export const Title = styled.p`
+  color: white;
+  font-size: 24px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+export const BorderLine = styled.div`
+  border: 1px solid #424242;
+`
---
+++ next-app/src/pages/myPage/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import * as S from './MyPage.style';
+
+const MyPage = () => {
+  return(
+  <S.Container>
+    <S.Contents>
+      <S.Title>내 정보</S.Title>
+      <S.BorderLine />
+      <S.BorderLine />
+    </S.Contents>
+  </S.Container>
+  )
+}
+
+export default MyPage;
