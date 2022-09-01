8f3a5a62bb4ebee2b5904b42cf538d24de31be01
saengmotmi <ohjtack@gmail.com>
2022-09-02 02:04:50 +0900

    add: 카드 컴포넌트 레이아웃 완료

---
+++ next-app/src/assets/icons/index.ts
@@ -0,0 +1,15 @@
+import NaverIcon from './naver_icon.svg'
+import DotsVertical from './dots_vertical.svg'
+import CardViewIcon from './card_view.svg'
+import GridViewIcon from './grid_view.svg'
+import Bookmark from './bookmark.svg'
+
+export const Icons = {
+  NaverIcon,
+  DotsVertical,
+  CardViewIcon,
+  GridViewIcon,
+  Bookmark,
+}
+
+export default Icons
--- next-app/src/components/views/Feeds/FeedsContainer.tsx
+++ next-app/src/components/views/Feeds/FeedsContainer.tsx
@@ -1,15 +1,33 @@
+import Image from 'next/image'
 import Card from 'components/common/Card'
+import Icons from 'assets/icons'
 import * as S from './FeedsContainer.style'
 
 const FeedsContainer = () => {
   return (
     <S.Container>
-      <h1>Feeds</h1>
-      <ul>
+      <S.Header>
+        <S.Title>홈 피드</S.Title>
+        <S.SelectViewType>
+          <S.ViewType
+            alt="카드 뷰"
+            src={Icons.CardViewIcon}
+            width={16}
+            height={16}
+          />
+          <S.ViewType
+            alt="그리드 뷰"
+            src={Icons.GridViewIcon}
+            width={16}
+            height={16}
+          />
+        </S.SelectViewType>
+      </S.Header>
+      <S.CardContainer>
         <Card />
         <Card />
         <Card />
-      </ul>
+      </S.CardContainer>
     </S.Container>
   )
 }
