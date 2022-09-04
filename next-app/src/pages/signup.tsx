d2f03d500055edd712f931eec45f9b7528e3f099
Yeongjae-Shin <syj9484@gmail.com>
2022-09-04 20:20:28 +0900

    feat: 폴더 구조 컨벤션에 맞춰 수정

---
+++ next-app/src/pages/signup.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import SignupContainerView from 'components/views/Signup/SignupContainer'
+
+const Signup: NextPage = () => {
+  return (
+    <div>
+      <SignupContainerView />
+    </div>
+  )
+}
+
+export default Signup
