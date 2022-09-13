7c571911f773c6fe06d69e41cf798a91a7d24803
seo <seo@reware.co.kr>
2022-09-13 20:52:19 +0900

    add : 레이아웃에 들어갈 이미지 추가(user는 추후 제거 예정)

--- next-app/src/assets/icons/index.ts
+++ next-app/src/assets/icons/index.ts
@@ -3,6 +3,8 @@ import DotsVertical from './dots_vertical.svg'
 import CardViewIcon from './card_view.svg'
 import GridViewIcon from './grid_view.svg'
 import Bookmark from './bookmark.svg'
+import Menu from './menu.svg'
+import Logo from './Feedoong.svg'
 
 export const Icons = {
   NaverIcon,
@@ -10,6 +12,8 @@ export const Icons = {
   CardViewIcon,
   GridViewIcon,
   Bookmark,
+  Menu,
+  Logo,
 }
 
 export default Icons
