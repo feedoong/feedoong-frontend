573add55c6e62fefc764b410873c05cd8cd0575e
seo <seo@reware.co.kr>
2022-09-17 15:31:03 +0900

    misc : 그리드 형태 스타일 파일 분리

---
+++ next-app/src/components/common/Gird/Grid.style.tsx
@@ -0,0 +1,55 @@
+import styled from 'styled-components'
+import { getTypographyStyles } from 'styles/fonts'
+
+export const Container = styled.div`
+  width: 300px;
+  height: 310px;
+  border-radius: 20px;
+  background-color: #f5f5f5;
+`
+
+export const ImageContainer = styled.div`
+  width: 100%;
+  height: 160px;
+  border-radius: 20px 20px 0 0;
+  background-color: #e0e0e0;
+`
+
+export const Contents = styled.div`
+  height: calc(310px - 160px);
+  padding: 15px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+`
+
+export const ContentTop = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .left {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    color: #8c8c8c;
+  }
+
+  .left .date {
+    ${getTypographyStyles('Body2_B')}
+  }
+
+  .left .date {
+    ${getTypographyStyles('Body2_M')}
+  }
+`
+
+export const ContentBody = styled.div`
+  font-size: 16px;
+`
+
+export const ContentFooter = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
--- next-app/src/components/common/Gird/Grid.tsx
+++ next-app/src/components/common/Gird/Grid.tsx
@@ -1,15 +1,14 @@
 import Image from 'next/image'
-import styled from 'styled-components'
 import Icons from 'assets/icons'
 import * as S from 'components/common/Card/Card.style'
-import { getTypographyStyles } from 'styles/fonts'
+import * as GS from 'components/common/Gird/Grid.style'
 
 const Grid = () => {
   return (
-    <Container>
-      <ImageContainer />
-      <Contents>
-        <ContentTop>
+    <GS.Container>
+      <GS.ImageContainer />
+      <GS.Contents>
+        <GS.ContentTop>
           <div className="left">
             <Image
               alt="네이버 로고"
@@ -28,11 +27,11 @@ const Grid = () => {
               height={16}
             />
           </div>
-        </ContentTop>
-        <ContentBody>
+        </GS.ContentTop>
+        <GS.ContentBody>
           제주 온 태국인 관광객 60% ‘입국 불허’…이탈자도 늘어
-        </ContentBody>
-        <ContentFooter>
+        </GS.ContentBody>
+        <GS.ContentFooter>
           <S.ReadStatus>읽음</S.ReadStatus>
           <S.Bookmark
             alt="북마크"
@@ -40,63 +39,10 @@ const Grid = () => {
             width={16}
             height={16}
           />
-        </ContentFooter>
-      </Contents>
-    </Container>
+        </GS.ContentFooter>
+      </GS.Contents>
+    </GS.Container>
   )
 }
 
 export default Grid
-
-const Container = styled.div`
-  width: 300px;
-  height: 310px;
-  border-radius: 20px;
-  background-color: #f5f5f5;
-`
-
-const ImageContainer = styled.div`
-  width: 100%;
-  height: 160px;
-  border-radius: 20px 20px 0 0;
-  background-color: #e0e0e0;
-`
-
-const Contents = styled.div`
-  height: calc(310px - 160px);
-  padding: 15px;
-  display: flex;
-  flex-direction: column;
-  gap: 25px;
-`
-
-const ContentTop = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  .left {
-    display: flex;
-    align-items: center;
-    gap: 5px;
-    color: #8c8c8c;
-  }
-
-  .left .date {
-    ${getTypographyStyles('Body2_B')}
-  }
-
-  .left .date {
-    ${getTypographyStyles('Body2_M')}
-  }
-`
-
-const ContentBody = styled.div`
-  font-size: 16px;
-`
-
-const ContentFooter = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`
