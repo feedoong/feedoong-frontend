d43cf04a076d859006e1b453bd6f6304059bb906
saengmotmi <ohjtack@gmail.com>
2022-10-10 14:49:47 +0900

    add: redirect env

--- next-app/src/components/views/SignUp/SignUpContainer.utils.ts
+++ next-app/src/components/views/SignUp/SignUpContainer.utils.ts
@@ -1,8 +1,10 @@
+import { getDomainName } from 'envs'
+
 const { NEXT_PUBLIC_GOOGLE_CLIENT_ID } = process.env
 const hostname = 'https://accounts.google.com/o/oauth2/v2/auth'
 
 const clientId = NEXT_PUBLIC_GOOGLE_CLIENT_ID!
-const redirectUri = 'http://localhost:3000/oauth'
+const redirectUri = `${getDomainName()}/oauth`
 const responseType = 'token'
 const scope =
   'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'
