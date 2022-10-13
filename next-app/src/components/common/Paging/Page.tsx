b14b5be441b1772a2abcb854d52e40de8e64ed74
Eunsun Shin <unusual0218@gmail.com>
2022-10-14 02:58:48 +0900

    WIP: Paging 작업 중

---
+++ next-app/src/components/common/Paging/Page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Page = () => {
+
+}
+
+export default Page;
---
+++ next-app/src/components/common/Paging/Paging.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Page from './Page'
+import * as S from './Paging.style'
+
+
+
+const Paging = () => {
+  return <div></div>
+}
+
+export default Paging
