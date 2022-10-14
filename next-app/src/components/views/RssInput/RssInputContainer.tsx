b49f04cfdd6cc04c61780899470739c50684b47c
eunsunshin <unusual0218@gmail.com>
2022-10-14 09:58:36 +0900

    Remove: 불필요한 코드 제거

--- next-app/src/components/views/RssInput/RssInputContainer.tsx
+++ next-app/src/components/views/RssInput/RssInputContainer.tsx
@@ -14,9 +14,6 @@ const RssInputContainer = () => {
 
   return (
     <S.Container>
-      <button onClick={() => Toast.show({ content: '테스트 해봅니다.'})}>
-        토스트
-      </button>
       <form
         onSubmit={(e) =>
           (!isPreviewLoading || isRssUrlValid(url)) && onSubmit(e)
