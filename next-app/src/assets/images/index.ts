09e9503fb3bcd268c930a1c85a1c5c111fcf2b8e
Eunsun Shin <unusual0218@gmail.com>
2022-10-30 01:31:28 +0900

    Add: custom404 error page 추가

---
+++ next-app/src/assets/images/index.ts
@@ -0,0 +1,7 @@
+import Custom404 from './404.svg'
+
+export const Images = {
+  Custom404
+}
+
+export default Images
---
+++ next-app/src/pages/404.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Custom404Container from 'components/views/404/404Container'
+
+const custom404 = () => {
+  return <Custom404Container />
+}
+
+export default custom404
