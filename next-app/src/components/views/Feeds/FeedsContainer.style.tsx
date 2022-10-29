ba7f9840f65c04252c55c94fb1d5eac006a91773
saengmotmi <ohjtack@gmail.com>
2022-10-30 02:35:15 +0900

    fix: 카드 컴포넌트 반응형 수정

--- next-app/src/components/views/Feeds/FeedsContainer.style.tsx
+++ next-app/src/components/views/Feeds/FeedsContainer.style.tsx
@@ -1,4 +1,4 @@
-import Image from "next/legacy/image";
+import Image from 'next/legacy/image'
 import styled, { css, type FlattenSimpleInterpolation } from 'styled-components'
 
 import { colors } from 'styles/colors'
@@ -6,7 +6,6 @@ import { getTypographyStyles } from 'styles/fonts'
 
 export const Container = styled.div`
   display: flex;
-  /* justify-content: center; */
   flex-direction: column;
   align-items: center;
 `
@@ -14,6 +13,7 @@ export const Container = styled.div`
 export const FeedWrapper = styled.div`
   margin: 40px 20px;
   max-width: 626px;
+  width: 100%;
   border-radius: 4px;
 `
 
@@ -62,7 +62,5 @@ const CardLayout: {
 }
 
 export const CardContainer = styled.ul<{ type?: 'card' | 'grid' }>`
-  min-width: 626px;
-
   ${({ type = 'card' }) => CardLayout[type]}
 `
