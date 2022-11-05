d7ab6d88bba6823c44302668e514523995cb8f97
Eunsun Shin <unusual0218@gmail.com>
2022-11-06 00:40:11 +0900

    Fix: fix lint error

--- next-app/src/hooks/useGoogleAnalytics.ts
+++ next-app/src/hooks/useGoogleAnalytics.ts
@@ -1,22 +1,22 @@
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import * as gtag from '../utils/gtag';
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
+import * as gtag from '../utils/gtag'
 
 const useGoogleAnalytics = () => {
-  const router = useRouter();
+  const router = useRouter()
 
   useEffect(() => {
     const handleRouteChange = (url: string) => {
-      gtag.pageview(url);
-    };
-    router.events.on('routeChangeComplete', handleRouteChange);
-    router.events.on('hashChangeComplete', handleRouteChange);
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
     return () => {
-      router.events.off('routeChangeComplete', handleRouteChange);
-      router.events.off('hashChangeComplete', handleRouteChange);
-    };
-  }, [router.events]);
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 }
 
-export default useGoogleAnalytics;
+export default useGoogleAnalytics
