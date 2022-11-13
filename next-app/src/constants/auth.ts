e73e0a889be61d45c25de9ad487f47fa693aabb1
saengmotmi <ohjtack@gmail.com>
2022-11-13 14:06:22 +0900

    token prefix 추가

--- next-app/src/constants/auth.ts
+++ next-app/src/constants/auth.ts
@@ -1,2 +1,4 @@
-export const AccessToken = 'accessToken'
-export const RefreshToken = 'refreshToken'
+const prefix = 'feedoong_'
+
+export const AccessToken = prefix + 'accessToken'
+export const RefreshToken = prefix + 'refreshToken'
