8a1cc431bd618f34a054eb76aa71c980f875db07
seo <seo@reware.co.kr>
2022-11-16 16:29:50 +0900

    add : 크롬 확장 페이지 추가 및 설정 변경(팝업, new-tab)

--- next-app/pages/popup-test.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function Popup() {
-  return <div>확장 프로그램에서 노출 되는 팝업창</div>
-}
-
-export default Popup
---
+++ next-app/src/pages/popup-test.tsx
@@ -0,0 +1,7 @@
+function Popup(){
+  return (
+    <>크롬 확장 팝업에 노출 되는 화면</>
+  );
+};
+
+export default Popup;
