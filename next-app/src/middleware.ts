3e1f3a69e94e762a11328ba26ac01743c38b7167
saengmotmi <ohjtack@gmail.com>
2022-11-27 02:56:21 +0900

    fix: 리프레시 토큰 만료 시 /introduce로 리다이렉트

--- next-app/src/middleware.ts
+++ next-app/src/middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse, type NextRequest } from 'next/server'
 
 import { isLoginValidServerSide } from 'utils/auth'
 
-export function middleware(request: NextRequest) {
+export function middleware(request: NextRequest, response: NextResponse) {
   if (!isLoginValidServerSide(request)) {
     return NextResponse.redirect(new URL('/introduce', request.url), {
       status: 302,
--- next-app/src/utils/auth.ts
+++ next-app/src/utils/auth.ts
@@ -1,7 +1,15 @@
 import type { NextRequest } from 'next/server'
+import Cookies from 'js-cookie'
 
 import { AccessToken, RefreshToken } from 'constants/auth'
+import api from 'services/api'
 
 export const isLoginValidServerSide = (request: NextRequest) => {
   return request.cookies.get(AccessToken) || request.cookies.get(RefreshToken)
+}
+
+export const destroyTokensClientSide = () => {
+  Cookies.remove(RefreshToken)
+  Cookies.remove(AccessToken)
+  api.defaults.headers.common['Authorization'] = ''
 }
