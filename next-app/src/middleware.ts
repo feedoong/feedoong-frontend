ee2d3eb03723809b1000bf34337322dcb27a14c8
saengmotmi <ohjtack@gmail.com>
2022-11-26 18:42:26 +0900

    미들웨어 수정

--- next-app/src/middleware.ts
+++ next-app/src/middleware.ts
@@ -1,5 +1,4 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { NextResponse, type NextRequest } from 'next/server'
 
 import { isLoginValidServerSide } from 'utils/auth'
 
@@ -7,10 +6,8 @@ export function middleware(request: NextRequest) {
   const isStatic = request.nextUrl.pathname.includes('/_next')
   const isLoginProcess = request.nextUrl.pathname.includes('/oauth')
 
-  if (!isStatic && !isLoginProcess) {
-    if (!isLoginValidServerSide(request)) {
-      return NextResponse.rewrite(new URL('/signup', request.url))
-    }
+  if (!isStatic && !isLoginProcess && !isLoginValidServerSide(request)) {
+    return NextResponse.rewrite(new URL('/signup', request.url))
   }
 
   return NextResponse.next()
