fd3a1015d39a23613c133f0ce0a83a33974a4e5d
Eunsun Shin <unusual0218@gmail.com>
2022-12-04 19:59:47 +0900

    Add: SideMenu 열려있을때 외부영역 누르면 사이드메뉴 닫히도록 기능 추가

--- next-app/src/components/common/Layout/Nav/Nav.tsx
+++ next-app/src/components/common/Layout/Nav/Nav.tsx
@@ -1,23 +1,41 @@
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 
 import TopNavBar from './TopNavBar'
 import SideMenuBar from './SideNavBar'
 
 const Nav = () => {
   const [showSideBar, setShowSideBar] = useState<boolean | null>(null)
+  const topNavRef = useRef<HTMLDivElement>(null)
+  const sideMenuRef = useRef<HTMLDivElement>(null)
 
-  const openSideBar = useCallback(() => {
-    setShowSideBar(true)
+  useEffect(() => {
+    document.addEventListener('click', onClickNavOutSide)
+
+    return () => {
+      document.removeEventListener('click', onClickNavOutSide)
+    }
   }, [])
 
-  const closeSideBar = useCallback(() => {
+  const onClickNavOutSide = (e: MouseEvent) => {
+    const target = e.target as Element
+    if (
+      target &&
+      (sideMenuRef.current?.contains(target) ||
+        topNavRef.current?.contains(target))
+    ) {
+      return
+    }
     setShowSideBar(false)
-  }, [])
+  }
 
   return (
     <>
-      <TopNavBar setShowSideBar={setShowSideBar} />
-      <SideMenuBar setShowSideBar={setShowSideBar} isOpen={showSideBar} />
+      <TopNavBar setShowSideBar={setShowSideBar} ref={topNavRef} />
+      <SideMenuBar
+        setShowSideBar={setShowSideBar}
+        isOpen={showSideBar}
+        ref={sideMenuRef}
+      />
     </>
   )
 }
