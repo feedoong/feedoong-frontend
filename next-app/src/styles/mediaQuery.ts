ab8086ad201679e65daf0720bdb9577a9219cc1a
saengmotmi <ohjtack@gmail.com>
2022-12-04 19:18:15 +0900

    add: 미디어쿼리 유틸 함수

---
+++ next-app/src/styles/mediaQuery.ts
@@ -0,0 +1,19 @@
+import { css, CSSProp } from 'styled-components'
+
+const breakpoints: {
+  [key: string]: number
+} = {
+  mobile: 0,
+  tablet: 768,
+  desktop: 1024,
+}
+
+export const DeviceQuery = Object.keys(breakpoints).reduce((acc, label) => {
+  acc[label] = (literals: TemplateStringsArray, ...placeholders: any[]) =>
+    css`
+      @media only screen and (max-width: ${breakpoints[label]}px) {
+        ${css(literals, ...placeholders)};
+      }
+    `
+  return acc
+}, {} as Record<keyof typeof breakpoints, (l: TemplateStringsArray, ...p: any[]) => CSSProp>)
