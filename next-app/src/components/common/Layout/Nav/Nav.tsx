df7035d7c36469ab39b7740159510beb00aca3bd
Eunsun Shin <unusual0218@gmail.com>
2022-12-04 22:03:49 +0900

    Remove: 외부영역 클릭시 사이드메뉴 닫히는 기능 제거

--- next-app/src/components/common/Layout/Nav/Nav.tsx
+++ next-app/src/components/common/Layout/Nav/Nav.tsx
@@ -1,41 +1,15 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react'
+import React, { useState } from 'react'
 
 import TopNavBar from './TopNavBar'
 import SideMenuBar from './SideNavBar'
 
 const Nav = () => {
   const [showSideBar, setShowSideBar] = useState<boolean | null>(null)
-  const topNavRef = useRef<HTMLDivElement>(null)
-  const sideMenuRef = useRef<HTMLDivElement>(null)
-
-  useEffect(() => {
-    document.addEventListener('click', onClickNavOutSide)
-
-    return () => {
-      document.removeEventListener('click', onClickNavOutSide)
-    }
-  }, [])
-
-  const onClickNavOutSide = (e: MouseEvent) => {
-    const target = e.target as Element
-    if (
-      target &&
-      (sideMenuRef.current?.contains(target) ||
-        topNavRef.current?.contains(target))
-    ) {
-      return
-    }
-    setShowSideBar(false)
-  }
 
   return (
     <>
-      <TopNavBar setShowSideBar={setShowSideBar} ref={topNavRef} />
-      <SideMenuBar
-        setShowSideBar={setShowSideBar}
-        isOpen={showSideBar}
-        ref={sideMenuRef}
-      />
+      <TopNavBar setShowSideBar={setShowSideBar} />
+      <SideMenuBar setShowSideBar={setShowSideBar} isOpen={showSideBar} />
     </>
   )
 }
