aef88e7b084ef901d709507b00dd7fc98513e4fc
Yeongjae-Shin <syj9484@gmail.com>
2022-12-11 20:07:37 +0900

    feat: 포스트 즉시 읽음 처리

--- next-app/src/components/common/FeedItem/hooks/useReadPost.ts
+++ next-app/src/components/common/FeedItem/hooks/useReadPost.ts
@@ -1,13 +1,18 @@
-import { useMutation } from '@tanstack/react-query'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
 
 import { CACHE_KEYS } from 'services/cacheKeys'
 import { submitViewedItem } from 'services/feeds'
-import type { Item } from 'types/feeds'
+import type { Feed, Item } from 'types/feeds'
 
 const useReadPost = (item: Item) => {
+  const client = useQueryClient()
+
   const { mutate: handleRead } = useMutation(
     CACHE_KEYS.viewItem(item.id),
-    submitViewedItem
+    submitViewedItem,
+    {
+      onSuccess: () => client.invalidateQueries(CACHE_KEYS.feeds),
+    }
   )
 
   return {
