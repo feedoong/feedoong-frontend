216eb3b1a4e0bdfbe3a785a92d664b0ad7dcdc86
eunsonny <eunsonnny@gmail.com>
2023-01-12 00:02:44 +0900

    Modify: FeedItem-CardType 스타일 변경

--- next-app/src/components/common/FeedItem/CardType.style.tsx
+++ next-app/src/components/common/FeedItem/CardType.style.tsx
@@ -5,18 +5,18 @@ import { getTypographyStyles, ellipsis } from 'styles/fonts'
 
 export const Container = styled.div`
   width: 100%;
-  background-color: ${colors.gray100};
+  background-color: ${colors.white};
   padding: 20px;
   display: flex;
   flex-direction: column;
   gap: 12px;
   border-radius: 20px;
+  border-bottom-left-radius: 0px;
 `
 
-export const Title = styled.h2<{ isViewed: boolean }>`
+export const Title = styled.h2`
   ${getTypographyStyles('Headline3_B')}
   ${ellipsis(1)}
 
   color: ${colors.gray800};
-  opacity: ${({ isViewed }) => (isViewed ? 0.5 : 1)};
 `
