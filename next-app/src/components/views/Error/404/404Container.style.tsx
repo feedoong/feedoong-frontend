41fb12bc7e56301d9be6fd3883cc8cfc2c44cba1
Yeongjae-Shin <syj9484@gmail.com>
2023-01-15 18:19:53 +0900

    feat: 에러 페이지 퍼블리싱 수정

--- next-app/src/components/views/Error/404/404Container.style.tsx
+++ next-app/src/components/views/Error/404/404Container.style.tsx
@@ -3,21 +3,8 @@ import styled from 'styled-components'
 import { colors } from 'styles/colors'
 import { getTypographyStyles } from 'styles/fonts'
 
-export const ContentWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin: 213px auto 0px;
-  gap: 20px;
-`
-
-export const MainDescription = styled.p`
-  ${getTypographyStyles('Headline1_B')}
-`
-
 export const SubDescription = styled.div`
   ${getTypographyStyles('Headline3_M')}
   color: ${colors.gray700};
   text-align: center;
 `
-
--- next-app/src/components/views/Error/404/404Container.tsx
+++ next-app/src/components/views/Error/404/404Container.tsx
@@ -5,34 +5,35 @@ import { useRouter } from 'next/router'
 import Button from 'components/common/Button'
 
 import * as S from './404Container.style'
+import * as CommonS from '../common.style'
 
 import Images from 'assets/images'
 
 const Custom404Container = () => {
   const router = useRouter()
   return (
-    <>
-      <S.ContentWrapper>
+    <CommonS.ContentWrapper>
+      <CommonS.ContentContainer>
         <Image src={Images.Custom404} alt="404Error" />
-        <S.MainDescription>
+        <CommonS.MainDescription>
           요청하신 페이지를 찾을 수 없습니다.
-        </S.MainDescription>
+        </CommonS.MainDescription>
         <S.SubDescription>
           방문하시려는 페이지의 주소가 잘못 입력되었거나,<br></br>
           페이지의 주소가 변경 혹은 삭제되어 요청하신 페이지를 찾을 수 없습니다.
           <br></br>
           입력하신 주소가 정확한지 다시 한번 확인해 주시기 바랍니다.
         </S.SubDescription>
-      </S.ContentWrapper>
+      </CommonS.ContentContainer>
       <Button
         onClick={() => router.push('/')}
         buttonStyle="secondary"
-        style={{ padding: '0px 16px', margin: '24px auto' }}
+        style={{ padding: '0px 16px', margin: '0 auto' }}
         size="medium"
       >
         메인으로 이동하기
       </Button>
-    </>
+    </CommonS.ContentWrapper>
   )
 }
 
