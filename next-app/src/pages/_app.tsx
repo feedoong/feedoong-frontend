f233f4f588439a199d95fa22d1f6aacbb8d57cb9
saengmotmi <ohjtack@gmail.com>
2023-01-24 19:44:25 +0900

    refactor: Providers 컴포넌트 분리

--- next-app/src/pages/_app.tsx
+++ next-app/src/pages/_app.tsx
@@ -1,33 +1,13 @@
 import type { AppProps } from 'next/app'
-import {
-  Hydrate,
-  QueryClientProvider,
-  QueryClient,
-} from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import { RecoilRoot } from 'recoil'
 
 import Layout from 'components/common/Layout'
 import Scripts from 'components/common/Scripts'
 import { useGoogleAnalytics } from 'utils/hooks'
-import { globalQueryErrorHandler } from 'features/errors/globalQueryErrorHandler'
+import Providers from 'components/common/Providers'
 
 import 'styles/reset.css'
 import 'styles/font.css'
 import 'react-loading-skeleton/dist/skeleton.css'
-
-const queryClient: QueryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retry: false,
-      retryOnMount: false,
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-
-      onError: (err: unknown) => globalQueryErrorHandler(err, queryClient),
-    },
-  },
-})
 
 function MyApp({ Component, pageProps }: AppProps) {
   useGoogleAnalytics()
@@ -35,16 +15,11 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Scripts />
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={pageProps.dehydratedState}>
-          <RecoilRoot>
-            <Layout>
-              <Component {...pageProps} />
-            </Layout>
-          </RecoilRoot>
-        </Hydrate>
-        <ReactQueryDevtools />
-      </QueryClientProvider>
+      <Providers pageProps={pageProps}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Providers>
     </>
   )
 }
