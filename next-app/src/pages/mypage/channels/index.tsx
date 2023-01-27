ce55ddac8ebff81646f62c2324ab089b3f7d2c62
eunsonny <eunsonnny@gmail.com>
2023-01-27 22:11:32 +0900

    Remove: 불필요한 코드 제거

--- next-app/src/pages/mypage/channels/index.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-
-import MyChannelView from 'components/views/MyChannel'
-
-const MyChannel: NextPage = () => {
-  return <MyChannelView />
-}
-
-export default MyChannel
