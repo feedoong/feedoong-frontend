ae43f9f79aff86aa44263d72d6b55f3644722666
saengmotmi <ohjtack@gmail.com>
2023-03-07 03:35:44 +0900

    add: 채널 추가 기능 구현 완료

--- next-app/src/components/common/FeedItem/SubscriptionType.tsx
+++ next-app/src/components/common/FeedItem/SubscriptionType.tsx
@@ -1,13 +1,21 @@
+import Image from 'next/image'
+
 import type { PrivateSubscription, Subscription } from 'types/subscriptions'
 import Flex from '../Flex'
 import Anchor from '../Anchor'
 import { getWellKnownChannelImg } from 'utils'
 import PrivateFeedItemPopover from './Popovers/PrivateFeedItemPopover'
-import FeedItemPopover from './Popovers/FeedItemPopover'
 import LogoIcon from '../LogoIcon'
 import { getDiameterByType } from './FeedItem.utils'
+import { submitRssUrl } from 'services/feeds'
+import { getAxiosError, isAxiosError } from 'utils/errors'
+import Toast from '../Toast'
+import { useGetUserProfile } from 'features/user/userProfile'
+import { useCheckLoginModal } from 'features/auth/checkLogin'
 
 import { Container, Title, Url } from './SubscriptionType.style'
+
+import Icons from 'assets/icons'
 
 type Props =
   | {
@@ -20,6 +28,29 @@ type Props =
     }
 
 const SubscriptionType = ({ type, item }: Props) => {
+  const { data: user } = useGetUserProfile()
+  const { handleOpen, renderModal } = useCheckLoginModal()
+
+  const addChannel = async (item: Subscription) => {
+    try {
+      if (!user) {
+        handleOpen()
+        return
+      }
+      await submitRssUrl({ url: item.url, feedUrl: item.feedUrl })
+
+      Toast.show({ content: '새로운 채널이 추가 되었습니다.' })
+    } catch (error) {
+      if (isAxiosError(error)) {
+        const errorMessage = getAxiosError(error).message
+        Toast.show({
+          type: 'error',
+          content: `채널 추가에 실패했습니다. ${errorMessage}`,
+        })
+      }
+    }
+  }
+
   return (
     <Container>
       <Flex gap={12}>
@@ -40,14 +71,25 @@ const SubscriptionType = ({ type, item }: Props) => {
             {type === 'subscription/private' ? (
               <PrivateFeedItemPopover item={item as Subscription} />
             ) : (
-              <FeedItemPopover item={item as PrivateSubscription} />
+              <Image
+                src={Icons.AddMono}
+                style={{ cursor: 'pointer' }}
+                onClick={() => addChannel(item)}
+                width={20}
+                height={20}
+                alt="채널 추가"
+                priority
+              />
             )}
           </Flex>
           <Anchor href={item.url} target="_blank" style={{ width: '90%' }}>
-            <Url>{item.description}</Url>
+            <Url>
+              {type === 'subscription/private' ? item.description : item.url}
+            </Url>
           </Anchor>
         </Flex>
       </Flex>
+      {renderModal()}
     </Container>
   )
 }
--- next-app/src/components/common/Modal/ModalLayout.tsx
+++ next-app/src/components/common/Modal/ModalLayout.tsx
@@ -6,7 +6,9 @@ import * as S from './ModalLayout.styles'
 import Icons from 'assets/icons'
 
 interface Props {
-  title: string
+  title?: string
+  size: 'small' | 'medium' | 'large'
+  hasHeader?: boolean
   onClose?: VoidFunction
   children?: React.ReactNode
   style?: React.CSSProperties
@@ -16,22 +18,28 @@ export const ModalLayout: React.FC<Props> = ({
   title,
   onClose,
   style,
+  size,
   children,
+  hasHeader = true,
 }) => {
   return (
-    <S.Container>
-      <S.Header align="center" justify="between">
-        <S.Title>{title}</S.Title>
-        <Image
-          style={{ cursor: 'pointer' }}
-          onClick={onClose}
-          src={Icons.Close}
-          alt="close-icon"
-          width="12"
-          height="12"
-        />
-      </S.Header>
-      <Divider />
+    <S.Container size={size}>
+      {hasHeader && (
+        <>
+          <S.Header align="center" justify="between">
+            <S.Title>{title}</S.Title>
+            <Image
+              style={{ cursor: 'pointer' }}
+              onClick={onClose}
+              src={Icons.Close}
+              alt="close-icon"
+              width="12"
+              height="12"
+            />
+          </S.Header>
+          <Divider />
+        </>
+      )}
       <S.Body style={style}>{children}</S.Body>
     </S.Container>
   )
