d8b673ec750709debb49258fa1c25516afe6844f
saengmotmi <ohjtack@gmail.com>
2023-03-26 17:07:05 +0900

    refactor: 미들웨어 리다이렉트 경로 변경

--- next-app/src/middleware.ts
+++ next-app/src/middleware.ts
@@ -4,18 +4,18 @@ import httpStatus from 'http-status-codes'
 import { ROUTE } from 'constants/route'
 import { isLoginValidServerSide } from 'utils/auth'
 
+// 정적인 경로에 대해서만 처리가 가능해서 정말 필요한 기능인지 고민해볼 것
+export const config = {
+  matcher: ['/feed/me', '/mypage/:path*'],
+}
+
 export function middleware(request: NextRequest, response: NextResponse) {
   if (!isLoginValidServerSide(request)) {
     return NextResponse.redirect(
-      new URL(ROUTE.RECOMMENDED_CHANNELS, request.url),
+      new URL(ROUTE.INTRODUCE, request.url),
       httpStatus.MOVED_TEMPORARILY
     )
   }
 
   return NextResponse.next()
-}
-
-export const config = {
-  // NOTE: 'features/auth/requiredAuthMatcher' 경로의 값을 사용할 경우 too_many_redirects 에러가 발생함
-  matcher: ['/', '/mypage/:path*'],
 }
