13e72fcc3e70ab5c4c5d797ba754875b17db58ba
saengmotmi <ohjtack@gmail.com>
2023-04-03 22:33:07 +0900

    fix: react query 전역 캐시 렌더링 로직 안쪽으로 넣어 ssr 세션 겹침 문제 해결

--- next-app/src/components/common/Providers/index.tsx
+++ next-app/src/components/common/Providers/index.tsx
@@ -4,6 +4,7 @@ import {
   QueryClient,
 } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { useState } from 'react'
 
 import { globalQueryErrorHandler } from 'features/errors/globalQueryErrorHandler'
 
@@ -12,19 +13,23 @@ interface Props {
   pageProps: any
 }
 
-const queryClient: QueryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      retryOnMount: false,
-      refetchOnWindowFocus: false,
-      refetchOnReconnect: false,
-
-      onError: (err: unknown) => globalQueryErrorHandler(err, queryClient),
-    },
-  },
-})
-
 const Providers = ({ pageProps, children }: Props) => {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retryOnMount: false,
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+
+            onError: (err: unknown) =>
+              globalQueryErrorHandler(err, queryClient),
+          },
+        },
+      })
+  )
+
   return (
     <QueryClientProvider client={queryClient}>
       <Hydrate state={pageProps.dehydratedState}>{children}</Hydrate>
