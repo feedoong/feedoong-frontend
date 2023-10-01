7159b031d5b8ac54485f770895e5d578980bf2c0
saengmotmi <ohjtack@gmail.com>
2023-10-01 20:24:24 +0900

    refactor: 불필요한 주석 제거

--- next-app/src/pages/introduce.tsx
+++ next-app/src/pages/introduce.tsx
@@ -1,12 +1,10 @@
 import type { NextPage } from 'next'
 
-// import Top from 'components/views/Introduce/Top'
 import IntroduceMain from 'components/views/Introduce'
 
 const Introduce: NextPage = () => {
   return (
     <>
-      {/* <Top /> */}
       <IntroduceMain />
     </>
   )
