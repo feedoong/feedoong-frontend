d830061d7b1ed9d324c4bde1cd73512fd31227a5
eunsonny <eunsonnny@gmail.com>
2023-10-02 22:39:09 +0900

    Fix: 잘못 주석처리 된 코드 주석해제

--- next-app/src/features/channel/index.ts
+++ next-app/src/features/channel/index.ts
@@ -1,11 +1,14 @@
 import type { QueryFilters } from '@tanstack/react-query'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
+import type { AxiosError } from 'axios'
 
 import Toast from 'components/common/Toast'
 import { CACHE_KEYS } from 'services/cacheKeys'
 import { submitRssUrl } from 'services/feeds'
 import { deleteChannel } from 'services/subscriptions'
 import type { Channel } from 'types/subscriptions'
+import type { ErrorBody } from 'utils/errors'
+import { getAxiosError } from 'utils/errors'
 
 export const useSubscribeChannel = () => {
   const client = useQueryClient()
@@ -18,20 +21,19 @@ export const useSubscribeChannel = () => {
   })
 }
 
-// deprecated....?
-// export const subscribeChannel = async (item: Channel) => {
-//   Toast.show({
-//     type: 'promise',
-//     fetchFn: submitRssUrl({ url: item.url, feedUrl: item.feedUrl }),
-//     content: '새로운 채널이 추가되었어요!',
-//     promiseContent: {
-//       loading: '채널을 등록중이에요',
-//       error: (err: AxiosError<ErrorBody, any>) =>
-//         `채널 추가에 실패했어요 😅 ${getAxiosError(err).message}`,
-//     },
-//     option: { duration: 3000 },
-//   })
-// }
+export const subscribeChannel = async (item: Channel) => {
+  Toast.show({
+    type: 'promise',
+    fetchFn: submitRssUrl({ url: item.url, feedUrl: item.feedUrl }),
+    content: '새로운 채널이 추가되었어요!',
+    promiseContent: {
+      loading: '채널을 등록중이에요',
+      error: (err: AxiosError<ErrorBody, any>) =>
+        `채널 추가에 실패했어요 😅 ${getAxiosError(err).message}`,
+    },
+    option: { duration: 3000 },
+  })
+}
 
 export const useUnsubscribeChannel = (
   item: Channel,
