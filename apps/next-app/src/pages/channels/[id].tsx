00a971af2f425053509379240a02997d7a0eab06
eunsonny <eunsonnny@gmail.com>
2024-02-11 17:53:17 +0900

    Fix: yarn workspace monorepo 구조로 변경

---
+++ apps/next-app/src/pages/channels/[id].tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from 'next'
+import ChannelDetailView from 'components/views/Channel/ChannelDetailContainer'
+
+const ChannelDetail: NextPage = () => {
+  return <ChannelDetailView />
+}
+
+export default ChannelDetail
--- next-app/src/pages/[userName].tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-
-import UserPageContainer from 'components/views/UserPage'
-
-const UserProfile: NextPage = () => {
-  return <UserPageContainer />
-}
-
-export default UserProfile
