8bb53b42fdd5a0aef9814abcd918be350562f1dd
eunsonny <eunsonnny@gmail.com>
2024-03-16 17:14:17 +0900

    Refactor: introduce 페이지 app dir로 이전

--- apps/next-app/src/app/introduce/page.tsx
+++ apps/next-app/src/app/introduce/page.tsx
@@ -1,8 +1,10 @@
 'use client'
 import IntroduceHeaderBanner from 'components/views/Introduce/IntroduceHeaderBanner'
 import IntroduceHeaderTitle from 'components/views/Introduce/IntroduceHeaderTitle'
-import * as S from './Introduce.style'
 import IntroduceFeature1 from 'components/views/Introduce/IntroduceFeature1'
+import * as S from './Introduce.style'
+import IntroduceFeature2 from 'components/views/Introduce/IntroduceFeature2'
+import IntroduceFeature3 from 'components/views/Introduce/IntroduceFeature3'
 
 const IntroducePage = () => {
   return (
@@ -11,6 +13,8 @@ const IntroducePage = () => {
       <IntroduceHeaderBanner />
       <S.FeatureWrapper>
         <IntroduceFeature1 />
+        <IntroduceFeature2 />
+        <IntroduceFeature3/>
       </S.FeatureWrapper>
     </>
   )
