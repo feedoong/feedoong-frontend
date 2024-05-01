8c9c1cf9337aaba13a37c162154f9c90c3315d52
eunsonny <eunsonnny@gmail.com>
2024-05-01 17:35:56 +0900

    Fix: 배포 스크립트 수정

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 
 
-docker stop jamessoun93/feedoong-frontend
-docker rm jamessoun93/feedoong-frontend
+docker stop feedoong-frontend
+docker rm feedoong-frontend
 
 docker pull jamessoun93/feedoong-frontend
 
-docker run -d -p 8080:3000 jamessoun93/feedoong-frontend
+docker run --name feedoong-frontend -d -p 3000:3000 jamessoun93/feedoong-frontend:latest
 
 echo "Deployment script executed successfully."
