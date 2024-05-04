ef9ab825dac478aad599099317920ba013324a64
eunsonny <eunsonnny@gmail.com>
2024-05-05 00:54:04 +0900

    Add: deploy 스크립트에 기존에 사용하던 도커 이미지 삭제하는 커멘드 추가

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 docker stop feedoong-frontend
 docker rm feedoong-frontend
 
+docker rmi $(docker images jamessoun93/feedoong-frontend -q)
+
 docker pull jamessoun93/feedoong-frontend
 
 docker run --name feedoong-frontend -d -p 3000:3000 jamessoun93/feedoong-frontend:latest
