22f692f3026cf3bf9231a9393fd7c63cc8698c63
eunsonny <eunsonnny@gmail.com>
2024-06-30 14:31:31 +0900

    Remove: 불필요한 import 제거

--- apps/next-app/src/components/views/Feeds/MyFeed/MyFeed.tsx
+++ apps/next-app/src/components/views/Feeds/MyFeed/MyFeed.tsx
@@ -8,7 +8,6 @@ import { getFeeds } from 'services/feeds'
 import FeedItem from 'components/common/FeedItem'
 import Loading from 'components/common/Loading'
 import * as S from '../FeedsContainer.style'
-import { PostFeedItem } from 'features/post/ui/PostFeedItem'
 
 const MyFeed = () => {
   const { data, fetchNextPage, hasNextPage, isFetchingNextPage, isFetching } =
