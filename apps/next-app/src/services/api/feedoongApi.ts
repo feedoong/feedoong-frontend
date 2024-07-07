10b8f9c06a413eb21081bad011e5687b5f6ea2d0
eunsonny <eunsonnny@gmail.com>
2024-07-07 22:01:17 +0900

    Chore: 타입에러 방지용 feedoongApi에 인자값 추가

--- apps/next-app/src/services/api/feedoongApi.ts
+++ apps/next-app/src/services/api/feedoongApi.ts
@@ -1,9 +1,13 @@
+import type { AxiosHeaders } from 'axios'
+
 import api from '.'
 
 type FeedoongApiArg = {
   url: string
-  method: 'get' | 'post'
+  method: 'get' | 'post' | 'delete' | 'put'
   params?: unknown
+  headers?: Partial<AxiosHeaders>
+  data?: unknown
 }
 
 export const feedoongApi = <TData>({ url, method, params }: FeedoongApiArg) => {
